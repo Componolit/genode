93d48afa72eead90ce1b186d57f14e60e87473f2
Johannes Kliemann <kliemann@componolit.com>
2018-05-24 10:30:11 +0200

    implement exception handlers in a-except

--- repos/libports/src/lib/ada/runtimelib/a-except_c.cc
+++ repos/libports/src/lib/ada/runtimelib/a-except_c.cc
@@ -1,8 +1,30 @@
 
 #include <base/log.h>
 #include <util/string.h>
+#include <ada/exception.h>
 
 extern "C" {
+
+    /* Program Error */
+    void __gnat_rcheck_PE_Explicit_Raise(char *file, int line)
+    {
+        Genode::error("Program Error in ", Genode::Cstring(file), " at line ", line);
+        throw Ada::Exception::Program_Error();
+    }
+
+    /* Constraint Error */
+    void __gnat_rcheck_CE_Explicit_Raise(char *file, int line)
+    {
+        Genode::error("Constraint Error in ", Genode::Cstring(file), " at line ", line);
+        throw Ada::Exception::Constraint_Error();
+    }
+
+    /* Storage Error */
+    void __gnat_rcheck_SE_Explicit_Raise(char *file, int line)
+    {
+        Genode::error("Storage Error in ", Genode::Cstring(file), " at line ", line);
+        throw Ada::Exception::Storage_Error();
+    }
 
     void raise_ada_exception(char *name, char *message)
     {
