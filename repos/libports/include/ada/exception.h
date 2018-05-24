b5d662a515ad64ffe59bc668b5d783e593d9374c
Johannes Kliemann <kliemann@componolit.com>
2018-05-24 10:52:34 +0200

    add constraint error subtypes

--- repos/libports/include/ada/exception.h
+++ repos/libports/include/ada/exception.h
@@ -6,5 +6,10 @@ namespace Ada {
         class Program_Error : Genode::Exception {};
         class Constraint_Error : Genode::Exception {};
         class Storage_Error : Genode::Exception {};
+
+        class Length_Check : Constraint_Error {};
+        class Overflow_Check : Constraint_Error {};
+        class Invalid_Data : Constraint_Error {};
+        class Range_Check : Constraint_Error {};
     };
 };
