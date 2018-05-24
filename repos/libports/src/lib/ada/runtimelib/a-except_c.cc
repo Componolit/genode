3c27d92375d01b9ec5dd31656ef600def9236c1b
Johannes Kliemann <kliemann@componolit.com>
2018-05-24 10:53:02 +0200

    implement constraint error subtype handlers

--- repos/libports/src/lib/ada/runtimelib/a-except_c.cc
+++ repos/libports/src/lib/ada/runtimelib/a-except_c.cc
@@ -26,6 +26,40 @@ extern "C" {
         throw Ada::Exception::Storage_Error();
     }
 
+    /* Constraint Error subtypes */
+
+    /* Length check failed */
+    void __gnat_rcheck_CE_Length_Check(char *file, int line)
+    {
+        Genode::error("Constraint Error: Length check failed in ",
+                Genode::Cstring(file), " at line ", line);
+        throw Ada::Exception::Length_Check();
+    }
+
+    /* Overflow check failed */
+    void __gnat_rcheck_CE_Overflow_Check(char *file, int line)
+    {
+        Genode::error("Constraint Error: Overflow check failed in ",
+                Genode::Cstring(file), " at line ", line);
+        throw Ada::Exception::Overflow_Check();
+    }
+
+    /* Invalid data */
+    void __gnat_rcheck_CE_Invalid_Data(char *file, int line)
+    {
+        Genode::error("Constraint Error: Invalid data in ",
+                Genode::Cstring(file), " at line ", line);
+        throw Ada::Exception::Invalid_Data();
+    }
+
+    /* Range check failed */
+    void __gnat_rcheck_CE_Range_Check(char *file, int line)
+    {
+        Genode::error("Constraint Error: Range check failed in ",
+                Genode::Cstring(file), " at line ", line);
+        throw Ada::Exception::Range_Check();
+    }
+
     void raise_ada_exception(char *name, char *message)
     {
         Genode::error(Genode::Cstring(name), " raised: ", Genode::Cstring(message));
